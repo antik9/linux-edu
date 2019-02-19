9199410385a8b55f2674bf637d598190c1805cab
antik9 <illarionovanton@gmail.com>
2019-02-19 21:53:15 +0300

    Create docker cluster for mysql. Almost entire project taken from: https://github.com/neumayer/mysql-docker-compose-examples/tree/master/innodb-cluster

---
+++ README.md
@@ -0,0 +1,5 @@
+## To run cluster
+```
+>>> docker-compose up
+>>> mysql example -h127.0.0.1 -P6446 -uantik -puoZjwLjeZcHY6tFk
+```
---
+++ scripts/db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE example;
+CREATE USER 'antik'@'%' IDENTIFIED BY 'uoZjwLjeZcHY6tFk';
+GRANT ALL PRIVILEGES ON example.* TO 'antik'@'%';
